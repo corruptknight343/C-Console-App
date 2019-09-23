a622b3115931a0cf97aea20ad37402940e1c14db
corruptknight343 <55381975+corruptknight343@users.noreply.github.com>
2019-09-23 09:58:17 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calculator";
+
+            //Variables
+            int Number1 = 23;
+            int Number2 = 10;
+            int Answer;
+
+            Console.WriteLine("Number 1: " + Number1);
+            Console.WriteLine("Number 2: " + Number2);
+            Answer = Number1 + Number2;
+            Console.WriteLine("\n" + Number1 + " + " + Number2 + " = " + Answer);
+            Answer = Number1 - Number2;
+            Console.WriteLine("\n" + Number1 + " - " + Number2 + " = " + Answer);
+            Answer = Number1 * Number2;
+            Console.WriteLine("\n" + Number1 + " x " + Number2 + " = " + Answer);
+            Answer = Number1 / Number2;
+            Console.WriteLine("\n" + Number1 + " / " + Number2 + " = " + Answer);
+            Console.ReadLine();
+
+
+        }
+    }
+}
